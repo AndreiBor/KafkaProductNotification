05dcb861572b20c8b97fca1b5fdd9b80286aed1d
Andrei Borisov <andrei.borr@gmail.com>
2024-04-06 17:30:03 +0300

    Final

---
+++ EmailNotificationMicroservice/src/main/java/by/javaguru/ws/emailnotification/persistence/model/ProcessedEventEntity.java
@@ -0,0 +1,56 @@
+package by.javaguru.ws.emailnotification.persistence.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "processed_events")
+public class ProcessedEventEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -1292818659450184629L;
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String messageId;
+
+    @Column(nullable = false)
+    private String productId;
+
+    public ProcessedEventEntity() {
+    }
+
+    public ProcessedEventEntity(String messageId, String productId) {
+        this.messageId = messageId;
+        this.productId = productId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+}
